1fd4e8eb06f078fe2826cd4dc1a1f2e274fc1e4c
Chardon François <francoischardon.contact@gmail.com>
2021-03-22 08:41:54 +0100

    loveu

--- src/form-view.php
+++ src/form-view.php
@@ -6,13 +6,26 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Ranchers&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Cormorant+Unicase:wght@300&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@1,100&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Della+Respira&display=swap" rel="stylesheet">
+      
+    <link rel="stylesheet" href="./style.css">
     <title>Order Pizzas & drinks</title>
 </head>
 <body>
-<div class="container">
-    <h1>Order pizzas in restaurant "the Personal Pizza Processors"</h1>
+
+
+<div class="container ">
+    <div class="police-head">
+    <h1 class="d-flex justify-content-center">Order pizzas in restaurant "the Personal Pizza Processors"</h1>
     <nav>
-        <ul class="nav">
+        <ul class="nav d-flex justify-content-center">
             <li class="nav-item">
                 <a class="nav-link active" href="?food=1">Order pizzas</a>
             </li>
@@ -21,55 +34,67 @@
             </li>
         </ul>
     </nav>
-    <form method="post">
-        <div class="form-row">
+</div>
+    <p class="text-white bg-success d-flex justify-content-center"><?php echo $confirmed;?></p>
+    <p class="text-white bg-danger d-flex justify-content-center" ><?php echo $refus;?></p>
+    <p class=" d-inline required ">* <span>required field</span></p>
+    <form class="police2" method="post" method="index.php" id="form">
+        <div class="form-row ">
             <div class="form-group col-md-6">
-                <label for="email">E-mail:</label>
-                <input type="text" id="email" name="email" class="form-control"/>
+                <label class="label" for="email">E-mail:</label>
+                <span class="error text-danger" >* <?php echo $emailErr;?></span>
+                <input type="text" id="email" name="email" class="form-control input" value = "<?php echo $email;?>" placeholder= "iLovePizza@outlook.be" />
             </div>
             <div></div>
         </div>
 
-        <fieldset>
-            <legend>Address</legend>
+        <fieldset >
+            <legend class="d-flex  title">Address</legend>
 
             <div class="form-row">
                 <div class="form-group col-md-6">
-                    <label for="street">Street:</label>
-                    <input type="text" name="street" id="street" class="form-control">
+                    <label class="label" for="street">Street:</label>
+                    <span class="error text-danger">* <?php echo $streetErr;?></span>
+                    <input type="text" name="street" id="street" class="form-control input" value= "<?php echo $street;?>" placeholder="Place de la gare " >
                 </div>
                 <div class="form-group col-md-6">
-                    <label for="streetnumber">Street number:</label>
-                    <input type="text" id="streetnumber" name="streetnumber" class="form-control">
+                    <label class=" label" for="streetnumber">Street number:</label>
+                    <span class="error text-danger">* <?php echo $streetNumberErr;?></span>
+                    <input type="text" id="streetnumber" name="streetnumber" class="form-control input" value= "<?php echo $streetNumber;?>" placeholder= " 54 " >
                 </div>
             </div>
             <div class="form-row">
                 <div class="form-group col-md-6">
-                    <label for="city">City:</label>
-                    <input type="text" id="city" name="city" class="form-control">
+                    <label class=" label" for="city">City:</label>
+                    <span class="error text-danger">* <?php echo $cityErr;?></span>
+                    <input type="text" id="city" name="city" class="form-control input" value= "<?php echo $city;?>" placeholder="Marche-En-Famene" >
                 </div>
                 <div class="form-group col-md-6">
-                    <label for="zipcode">Zipcode</label>
-                    <input type="text" id="zipcode" name="zipcode" class="form-control">
+                    <label class="label" for="zipcode">Zipcode</label>
+                    <span class="error text-danger">* <?php echo $zipcodeErr;?></span>
+                    <input type="text" id="zipcode" name="zipcode" class="form-control input" value= "<?php echo $zipcode;?>" placeholder="6900">
                 </div>
             </div>
         </fieldset>
 
         <fieldset>
-            <legend>Products</legend>
+            <legend class="title">Products</legend>
             <?php foreach ($products AS $i => $product): ?>
-                <label>
-                    <input type="checkbox" value="1" name="products[<?php echo $i ?>]"/> <?php echo $product['name'] ?> -
-                    &euro; <?php echo number_format($product['price'], 2) ?></label><br />
+                <label class="police">
+                    <input type="checkbox" min="0" max="100" value="0" name="products[<?php echo $i ?>]"/> <?php echo $product['name'] ?> -
+                    &euro; <?php echo number_format($product['price'], 2)?></label><br />
             <?php endforeach; ?>
+
         </fieldset>
         
-        <label>
+        
+        <label class="police">
             <input type="checkbox" name="express_delivery" value="5" /> 
             Express delivery (+ 5 EUR) 
         </label>
-            
-        <button type="submit" class="btn btn-primary">Order!</button>
+        <div class="ma-div">
+            <button type="submit" class="btn btn-success btn-submit">Order!</button>
+        </div>
     </form>
 
     <footer>You already ordered <strong>&euro; <?php echo $totalValue ?></strong> in pizza(s) and drinks.</footer>
@@ -80,5 +105,6 @@
         text-align: center;
     }
 </style>
+
 </body>
 </html>
--- src/index.php
+++ src/index.php
@@ -16,28 +16,141 @@ function whatIsHappening() {
     var_dump($_SESSION);
 }
 
-//your products with their price.
-$products = [
-    ['name' => 'Margherita', 'price' => 8],
-    ['name' => 'Hawaï', 'price' => 8.5],
-    ['name' => 'Salami pepper', 'price' => 10],
-    ['name' => 'Prosciutto', 'price' => 9],
-    ['name' => 'Parmiggiana', 'price' => 9],
-    ['name' => 'Vegetarian', 'price' => 8.5],
-    ['name' => 'Four cheeses', 'price' => 10],
-    ['name' => 'Four seasons', 'price' => 10.5],
-    ['name' => 'Scampi', 'price' => 11.5]
-];
+//validate form
+$emailErr = $streetErr = $streetNumberErr = $cityErr = $zipcodeErr = '';
+$email = $street = $streetNumber = $city = $zipcode = '' ;
 
-$products = [
-    ['name' => 'Water', 'price' => 1.8],
-    ['name' => 'Sparkling water', 'price' => 1.8],
-    ['name' => 'Cola', 'price' => 2],
-    ['name' => 'Fanta', 'price' => 2],
-    ['name' => 'Sprite', 'price' => 2],
-    ['name' => 'Ice-tea', 'price' => 2.2],
-];
+$confirmed = " ";
+$refus = " ";
 
-$totalValue = 0;
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST['email'])) {
+      $emailErr = "E-mail is required";
+    } else {
+      $email= input($_POST['email']);
+      if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $emailErr = "Invalid email format";
+      }
+    }
+    if (empty($_POST['street'])) {
+        $streetErr = "Street is required";
+      } else {
+        $street= input($_POST['street']);
+        if (!preg_match("/^[a-zA-Z-' ]*$/",$street)) {
+            $streetErr = "Only letters and white space allowed";
+          }
+      }
+    if (empty($_POST['streetnumber'])) {
+        $streetNumberErr = "Street number is required";
+      } else {
+        $streetNumber= input($_POST['streetnumber']);
+        if(!ctype_digit($streetNumber)){
+            $streetNumberErr = 'only number allowed';
+        }
 
-require 'form-view.php';
+      }
+    if (empty($_POST['city'])) {
+        $cityErr = "City number is required";
+      } else {
+        $city= input($_POST['city']);
+        if (!preg_match("/^[a-zA-Z-' ]*$/",$city)) {
+            $cityErr = "Only letters and white space allowed";
+          }
+      }
+      if (empty($_POST['zipcode'])) {
+        $zipcodeErr = " Zipcode is required";
+      } else {
+        $zipcode= input($_POST['zipcode']);
+        if(!ctype_digit($zipcode)){
+            $zipcodeErr = 'only number allowed';
+        }
+      }
+      if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+          $refus = "votre commande n'a pas été validé";
+          
+          
+        }elseif(!preg_match("/^[a-zA-Z-' ]*$/",$street)){
+            $refus = "votre commande n'a pas été validé";
+            
+        }elseif(!ctype_digit($streetNumber)){
+            $refus = "votre commande n'a pas été validé";
+        }elseif(!preg_match("/^[a-zA-Z-' ]*$/",$city)){
+            $refus = "votre commande n'a pas été validé";
+        }elseif(!ctype_digit($zipcode)){
+            $refus = "votre commande n'a pas été validé";
+        }else{
+            $confirmed = 'commande confirmed an email has been sent to you.';
+            
+            
+        }
+    }
+        
+        
+            
+        
+            
+   
+
+        
+       
+        
+  
+
+
+function input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+  }
+
+
+
+
+
+  
+  
+  
+  
+  //your products with their price.
+  $pizza = [
+      ['name' => 'Margherita', 'price' => 8],
+      ['name' => 'Hawaï', 'price' => 8.5],
+      ['name' => 'Salami pepper', 'price' => 10],
+      ['name' => 'Prosciutto', 'price' => 9],
+      ['name' => 'Parmiggiana', 'price' => 9],
+      ['name' => 'Vegetarian', 'price' => 8.5],
+      ['name' => 'Four cheeses', 'price' => 10],
+      ['name' => 'Four seasons', 'price' => 10.5],
+      ['name' => 'Scampi', 'price' => 11.5]
+    ];
+    
+    $drink = [
+        ['name' => 'Water', 'price' => 1.8],
+        ['name' => 'Sparkling water', 'price' => 1.8],
+        ['name' => 'Cola', 'price' => 2],
+        ['name' => 'Fanta', 'price' => 2],
+        ['name' => 'Sprite', 'price' => 2],
+        ['name' => 'Ice-tea', 'price' => 2.2],
+    ];
+    
+    $totalValue = 0;
+    //switch de page 
+    $products = $pizza;
+    if(isset($_GET['food'])){
+        if($_GET['food'] == false){
+            $products = $drink;
+        }
+    }
+    
+    //calcul price
+    
+    if(isset($_POST['products'])){
+        foreach($_POST['products'] as $i => $product){
+            $totalValue += $products[$i]['price'];
+        }
+    }
+    if(isset($_POST['express_delivery'])){
+        $totalValue += $_POST['express_delivery'];
+    }
+    require 'form-view.php';
